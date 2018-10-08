60aa9384a8bfba0c49a993ea86f041aa83b4a02c
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2018-10-08 15:15:25 -0600

    Menu de usuario y progra defensiva basica

--- instagram/home/management/commands/create_post.py
+++ instagram/home/management/commands/create_post.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from home.models import Post
+from home.models import Post, PostUserLike
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 import os
@@ -9,6 +9,7 @@ def cls():
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
+        cls()
         while(True):
             print("MENU\n------\n1. Crear usuario\n2. Listar usuarios\n3. Acceder\n4. Salir")
             option = input()
@@ -64,17 +65,51 @@ def user_menu(user):
         print("MENU DE {}\n----------------\n3.1. Crear Post\n3.2. Like de Post\n3.3. Delete Post\n3.4. Menu principal".format(user.first_name))
         option = input()
         if option == '1':
-                create_post(user)
+            create_post(user)
         elif option == '2':
-                list_users()
+            like_post(user)
         elif option == '3':
-            current_user = login()
-            if current_user:
-                user_menu(current_user)
+            delete(user)
         elif option == '4':
-            print("Goodbye Lad")
+            cls()
+            break
         else:
             pass
+
+def like_post(us):
+    cls()
+    posts = Post.objects.all()
+    if posts:
+        for post in posts:
+            print("pk={} {} \n {} \n by: {}".format(post.id, post.title, post.text, post.created_by))
+        print("\nPor favor ingrese el numero del post al que quiere dar like:")
+        pid = input()
+        if not pid:
+            input("No ha ingresado nada, regresando al menu")
+        else:
+            like = PostUserLike(user = us, post = posts[int(pid) - 1])
+            like.save()
+    else:
+        input("No existe ningun post")
+    cls()
+
+def delete(user):
+    posts = Post.objects.filter(created_by=user)
+    if posts:
+        for post in posts:
+            print("pk={} {} \n {} \n by: {}".format(post.id, post.title, post.text, post.created_by))
+        print("\nPor favor ingrese el numero del post que quiere borrar")
+        pid = input()
+        post = Post.objects.get(pk=int(pid))
+        seguro = input("borrando {} pk={}. Y/N: ".format(post.title, post.id))
+        if seguro == "y" or seguro == "Y":
+            post.delete()
+        else:
+            print("Cancelando...")
+    else:
+        input("No ha hecho ningun post")
+    cls()
+    
 
 def create_post(user):
     cls()
